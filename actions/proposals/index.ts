4391673a374677a2a90dc790602345a4d305cf94
sergioldr <sergio@evmt.io>
2024-04-03 00:01:55 +0200

    Add decimals field to token select in findInvoiceByProposalId function

--- actions/proposals/index.ts
+++ actions/proposals/index.ts
@@ -205,6 +205,7 @@ export const findInvoiceByProposalId = async (id: string) => {
       token: {
         select: {
           symbol: true,
+          decimals: true,
         },
       },
     },
